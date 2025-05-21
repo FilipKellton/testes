56919d5f95396dbc3b9de89d3c1d60192b92fce6
Gmyzo, Filip <filip.gmyzo+zoetis@zoetis.com>
2025-05-21 13:45:19 +0200

    solution 4

--- NetLead/Helpers/WordFrequencyAnalyzer.cs
+++ NetLead/Helpers/WordFrequencyAnalyzer.cs
@@ -1,6 +1,36 @@
-﻿namespace NetLead.Helpers;
+﻿using System.Text.RegularExpressions;
+
+namespace NetLead.Helpers;
 
 public class WordFrequencyAnalyzer
 {
-    public List<(string Word, int Count)> GetTopWords(string text, int topN) => throw new NotImplementedException();
+    public List<(string Word, int Count)> GetTopWords(string text, int topN)
+    {
+        if (string.IsNullOrWhiteSpace(text) || topN <= 0)
+        {
+            return [];
+        }
+
+        var words = Regex.Matches(text.ToLower(), @"\b[a-z]+\b").Select(match => match.Value);
+
+        var wordCounts = new Dictionary<string, int>();
+        foreach (var word in words)
+        {
+            if (wordCounts.TryGetValue(word, out int value))
+            {
+                wordCounts[word] = ++value;
+            }
+            else
+            {
+                wordCounts[word] = 1;
+            }
+        }
+
+        return wordCounts
+            .OrderByDescending(kv => kv.Value)
+            .ThenBy(kv => kv.Key)
+            .Take(topN)
+            .Select(kv => (kv.Key, kv.Value))
+            .ToList();
+    }
 }
--- NetLeadTests/WordFrequencyAnalyzerTests.cs
+++ NetLeadTests/WordFrequencyAnalyzerTests.cs
@@ -1,7 +1,53 @@
-﻿using NetLead.Helpers;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Routing;
+using NetLead.Helpers;
 
 namespace NetLeadTests;
 public class WordFrequencyAnalyzerTests
 {
     private readonly WordFrequencyAnalyzer _sut = new();
+
+    [Fact]
+    public void Returns_Top_3_Words_Correctly()
+    {
+        var result = _sut.GetTopWords("The quick brown fox jumps over the lazy dog. The dog was not amused.", 3);
+
+        var expected = new List<(string, int)>
+        {
+            ("the", 3),
+            ("dog", 2),
+            ("quick", 1)
+        };
+
+        result[0].Should().BeEquivalentTo(expected[0]);
+        result[1].Should().BeEquivalentTo(expected[1]);
+    }
+
+    [Fact]
+    public void Handles_Case_Insensitivity()
+    {
+        var result = _sut.GetTopWords("Apple apple APPLE", 1);
+        result[0].Should().BeEquivalentTo(("apple", 3));
+    }
+
+    [Fact]
+    public void Ignores_Punctuation()
+    {
+        var result = _sut.GetTopWords("Hello, world! Hello...", 1);
+        result[0].Should().BeEquivalentTo(("hello", 2));
+    }
+
+    [Fact]
+    public void Returns_Empty_List_On_Empty_Input()
+    {
+        var result = _sut.GetTopWords("", 5);
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Returns_All_Words_When_Top_N_Exceeds_Count()
+    {
+        var result = _sut.GetTopWords("One two three", 10);
+        result.Count.Should().Be(3);
+    }
 }
